fcf6c8902214c35f3b6bce22336e49359d960356
KIRAN V K <75604208+kvk-code@users.noreply.github.com>
2023-06-09 06:59:39 +0530

    Update SimpleBank.sol

--- SimpleBank.sol
+++ SimpleBank.sol
@@ -1,16 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
 contract SimpleBank {
     uint256 public balance;
+
+    // Initialize the balance at contract deployment
     constructor() {
         balance = 0;
     }
+
     function deposit(uint256 amount) public {
         balance += amount;
     }
+
     function withdraw(uint256 amount) public {
+        // Ensure there are enough funds to withdraw
         if (balance >= amount) {
             balance -= amount;
         }
     }
+
     function viewBalance() public view returns (uint256) {
         return balance;
     }
